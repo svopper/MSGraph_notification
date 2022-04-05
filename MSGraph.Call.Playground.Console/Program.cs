85e189468c65fa9feb8cac07fb38a2b13349a0ba
Kasper Olsen <kasper@kols.dk>
2022-04-05 15:30:49 +0200

    fixed error

--- MSGraph.Call.Playground.Console/Program.cs
+++ MSGraph.Call.Playground.Console/Program.cs
@@ -7,10 +7,10 @@ class Program
     {
         var graphService = new GraphService("client_id", "client_secret");
 
-        //var callRecord = await graphClient.GetCallRecord("0bef90f1-fc05-425e-8d32-0de5bb57ae2f");
-        await graphClient.CreateSubscription();
-        //await graphClient.UpdateSubscription(new Guid("26c59458-a30d-464f-883d-04fbce1cfd21"), DateTimeOffset.Now.AddHours(2));
-        //await graphClient.DeleteSubscriptions();
+        //var callRecord = await graphService.GetCallRecord("0bef90f1-fc05-425e-8d32-0de5bb57ae2f");
+        await graphService.CreateSubscription();
+        //await graphService.UpdateSubscription(new Guid("26c59458-a30d-464f-883d-04fbce1cfd21"), DateTimeOffset.Now.AddHours(2));
+        //await graphService.DeleteSubscriptions();
 
         System.Console.WriteLine("done");
     }
